ec0b3ff453fe454dff3c5e7c101d55c1cee4ae81
Aydin Kuru <aydinkuru81@gmail.com>
2022-05-05 20:50:01 +0200

    vue fe208 issues solved

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,11 +1,9 @@
-<template>
-  <div id="app">
-    <nav>
-      <router-link to="/">home</router-link> |
-      <router-link to="/about">about</router-link>
-    </nav>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app
+    #nav
+      router-link(to="/") Home |&nbsp;
+      router-link(to="/about") About
+    router-view
 </template>
 
 <style lang="scss">
@@ -13,11 +11,10 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 
   a {
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,29 +1,28 @@
-<template>
-  <div class="Home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/helloworld.vue'
 import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    UserCard,
   },
   data() {
     return {
       users: [],
+      time: new Date(),
     }
   },
   async created() {
     const usersRequest = await axios.get('/api/users')
-
     this.users = usersRequest.data
   },
 }
 </script>
+
+<template lang="pug">
+  .home
+    h1 Picories
+    p The time is: {{ time }}
+    user-card(v-for="user in users" :user="user")
+</template>
